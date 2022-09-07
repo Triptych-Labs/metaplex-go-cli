88d3ba8f8df60610802c4932d065be730ebecb31
Dominic DiGiacomo <ddigiacomo@triptychlabs.io>
2022-09-07 01:13:54 -0700

    rip genesysgo

--- src/utils/constants.go
+++ src/utils/constants.go
@@ -2,7 +2,7 @@ package utils
 
 const DEVNET = "https://devnet.genesysgo.net"
 const TESTNET = "https://api.testnet.solana.com"
-const MAINNET = "https://holy-summer-sunset.solana-mainnet.quiknode.pro/efc2e8c00b2cab60d4d8d21574890d60dd4eaeb6/"
+const MAINNET = "https://ssc-dao.genesysgo.net"
 const NETWORK = MAINNET
 
 const CDN = "https://triptychlabs.io:4445"
